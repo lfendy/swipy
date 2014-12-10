38f5b27946fa4f7cfb1143ab68e3d184593aee81
Joe Sustaric <jsustari@thoughtworks.com>
2014-12-10 20:38:57 +1100

    no abi's

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-echo no | android create avd --force -n test -t android-20 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-20 
 emulator -avd test -no-skin -no-audio -no-window &
 ./android-wait-for-emulator # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
 adb shell input keyevent 82 &
