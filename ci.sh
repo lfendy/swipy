633ca1c932d0c2d798a3b2bc6c346e62b57a529a
Liauw Fendy <lfendy@gmail.com>
2014-12-17 20:11:04 +1100

    gradle clean test on ci.sh

--- ci.sh
+++ ci.sh
@@ -2,12 +2,5 @@
 
 set -ex
 
-echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
-emulator -avd test -no-skin -no-audio -no-window &
-./android-wait-for-emulator # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
-adb shell input keyevent 82 &
-
-
-
 #run tests
-./gradlew connectedAndroidTest
+./gradlew clean test
