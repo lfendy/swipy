dcb22d75ac4e7f8e8ecfe67eaf849773a6552e0b
Joe Sustaric <jsustari@thoughtworks.com>
2015-02-26 14:04:27 +1100

    going back to old school build

--- ci.sh
+++ ci.sh
@@ -2,5 +2,16 @@
 
 set -ex
 
+#run tests - Roboelectric with no avd
+#./gradlew clean localTest --stacktrace --debug
+
+
+echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+./android-wait-for-emulator # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
+adb shell input keyevent 82 &
+
+
+
 #run tests
-./gradlew clean localTest --stacktrace --debug
+./gradlew connectedAndroidTest
